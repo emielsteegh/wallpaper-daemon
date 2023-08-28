ed25f78fa5654e984f29f052df6b9c6eaa58897f
Emiel Steegh <emiel.steegh@vodafoneziggo.com>
2023-08-28 10:27:09 +0200

    wallpaper write permissions

--- set_background.sh
+++ set_background.sh
@@ -21,22 +21,30 @@ author=$(xattr -p com.author "$WALLPAPER_PATH" 2>/dev/null || echo "NA")
 
 # if I am not the author of the current wallpaper, set a new one
 if [ $FORCE = true ] || [ $author != "wallpaper_daemon" ]; then
+    
+    # give write permissions to wallpaper path
+    chmod a+w $WALLPAPER_PATH
+
+    # look for all jpg files in wallpaper-storage
     shopt -s nullglob
     files=($WALLPAPER_STORE/*.{jpg,jpeg})
     shopt -u nullglob
 
+    # error if no wallpapers are found
     if [ ${#files[@]} -eq 0 ]; then
         echo "No wallpapers found in $WALLPAPER_STORE"
         exit 1
     fi
 
+    # choose a random wallpaper
     file=${files[$((RANDOM % ${#files[@]}))]}
 
+    # copy wallpaper to destination
     cp "$file" "$WALLPAPER_PATH"
 
-    # mark myself as author
+    # mark the daemon as author
     xattr -w com.author "wallpaper_daemon" "$WALLPAPER_PATH"
 
-    # refreshes background
+    # refresh background
     killall Dock 
 fi
