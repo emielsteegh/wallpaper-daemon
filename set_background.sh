f923a329b77337c408d254f19355c8e778ce05da
Emiel Steegh <emiel.steegh@vodafoneziggo.com>
2023-08-31 12:52:08 +0200

    md5 check

--- set_background.sh
+++ set_background.sh
@@ -16,25 +16,29 @@ WALLPAPER_PATH="/Library/Desktop/Wallpaper.jpg"
 BASE_DIR=$(dirname "$0")
 WALLPAPER_STORE="$BASE_DIR/wallpaper-storage"
 
-# check author attribute, if there is none, supress error and use "no" as default
-author=$(xattr -p com.author "$WALLPAPER_PATH" 2>/dev/null || echo "NA")
+# give write permissions to wallpaper path
+chmod a+w $WALLPAPER_PATH
 
-# if I am not the author of the current wallpaper, set a new one
-if [ $FORCE = true ] || [ $author != "wallpaper_daemon" ]; then
-    
-    # give write permissions to wallpaper path
-    chmod a+w $WALLPAPER_PATH
+# look for all jpg files in wallpaper-storage
+shopt -s nullglob
+files=($WALLPAPER_STORE/*.{jpg,jpeg})
+shopt -u nullglob
 
-    # look for all jpg files in wallpaper-storage
-    shopt -s nullglob
-    files=($WALLPAPER_STORE/*.{jpg,jpeg})
-    shopt -u nullglob
+# error if no wallpapers are found
+if [ ${#files[@]} -eq 0 ]; then
+    echo "No wallpapers found in $WALLPAPER_STORE"
+    exit 1
+fi
 
-    # error if no wallpapers are found
-    if [ ${#files[@]} -eq 0 ]; then
-        echo "No wallpapers found in $WALLPAPER_STORE"
-        exit 1
-    fi
+# get checksum of all files in wallpaper-storage
+for i in "${!files[@]}"; do
+    md5s[$i]=$(md5 -q "${files[$i]}")
+done
+
+current_md5=$(md5 -q "$WALLPAPER_PATH")
+
+# if force or current md5 not in md5s
+if [ $FORCE = true ] || ! [[ ${md5s[@]} =~ ${current_md5} ]]; then
 
     # choose a random wallpaper
     file=${files[$((RANDOM % ${#files[@]}))]}
